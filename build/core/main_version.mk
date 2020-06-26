1e83e30de67a67030005e9418e9c316a37506e9c
alanndz <alanmahmud0@gmail.com>
2020-06-26 19:39:39 +0530

    Rebrand to Bianca
    Signed-off-by: alanndz <alanmahmud0@gmail.com>
    Change-Id: I13cc9f0fd85e627eeee718d8edd75313bb99a367

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -7,10 +7,9 @@ endif
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.lineage.releasetype=$(BIANCA_BUILDTYPE) \
+    ro.lineage.build.version=v$(VERSION) \
+    ro.modversion=$(LINEAGE_VERSION)
 
 # LineageOS Platform Display Version
 ADDITIONAL_BUILD_PROPERTIES += \
