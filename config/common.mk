86d6aa4fa7ea640c07a4fe6f7eecd3e46775410e
alanndz <alanmahmud0@gmail.com>
2020-07-06 01:44:42 +0530

    Don't build Updater
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -124,8 +124,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard
 
 # Themes
 PRODUCT_PACKAGES += \
