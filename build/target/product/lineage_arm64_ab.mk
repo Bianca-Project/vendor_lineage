cd9724c2c631b4a022064fb7c280f1f3bc8abcc0
Nolen Johnson <johnsonnolen@gmail.com>
2021-03-09 14:10:39 -0500

    lineage: targets: Add support for Legacy GSIs
    * This is needed for VNDK27, and other older,
      hackier Treble implementations.
    
    * We can't include `legacy_gsi_release.mk` as it's included
      in the `aosp_${ARCH}_ab` targets themselves, unlike the standard
      `aosp_${ARCH}` targets which guard `gsi_release.mk`'s inclusion
       with `aosp_${ARCH}` product guards.
    
    Change-Id: I16c5983c18110b24eef84e382e0ae6852efb03c3

--- build/target/product/lineage_arm64_ab.mk
+++ build/target/product/lineage_arm64_ab.mk
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_x86_64.mk)
-$(call inherit-product, build/target/product/gsi_release.mk)
+$(call inherit-product, build/target/product/aosp_arm64_ab.mk)
 
-include vendor/bianca/build/target/product/lineage_generic_target.mk
+include vendor/lineage/build/target/product/lineage_generic_target.mk
 
-PRODUCT_NAME := lineage_x86_64
+TARGET_NO_KERNEL_OVERRIDE := true
 
-PRODUCT_SDK_ADDON_NAME := lineage
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
+PRODUCT_NAME := lineage_arm64_ab
--- build/target/product/lineage_x86_64_ab.mk
+++ build/target/product/lineage_x86_64_ab.mk
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_x86_64.mk)
-$(call inherit-product, build/target/product/gsi_release.mk)
+$(call inherit-product, build/target/product/aosp_x86_64_ab.mk)
 
-include vendor/bianca/build/target/product/lineage_generic_target.mk
+include vendor/lineage/build/target/product/lineage_generic_target.mk
 
-PRODUCT_NAME := lineage_x86_64
+PRODUCT_NAME := lineage_x86_64_ab
 
 PRODUCT_SDK_ADDON_NAME := lineage
 PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
