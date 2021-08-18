LineageOS/android_vendor_lineage

lineage: Add framework compatibility matrix for Lineage HALs.
 * Cover all possible LOS HALs here instead of covering
   them in the device specific manifests.

Change-Id: Id7f7d6eb1f14190a061537cebfabd339af9b1620

config/common.mk
@@ -189,6 +189,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+
 PRODUCT_VERSION_MAJOR = 18
 PRODUCT_VERSION_MINOR = 1
 PRODUCT_VERSION_MAINTENANCE := 0

Bianca-Project/vendor_lineage

 * Cover all possible Lineage HALs here instead of covering

config/common.mk
@@ -199,6 +199,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/bianca/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/bianca/overlay/common
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/bianca/config/device_framework_matrix.xml
+
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/bianca/build/target/product/security/lineage
 