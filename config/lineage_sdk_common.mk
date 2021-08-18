LineageOS/android_vendor_lineage

config: Add globalactions service permissions
The service won't start without this.

Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
Change-Id: I377a45aceb76d0a94504ef799f9c33782185ca1f

config/lineage_sdk_common.mk
@@ -2,6 +2,7 @@
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.audio.xml \
     vendor/lineage/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.hardware.xml \
+    vendor/lineage/config/permissions/org.lineageos.globalactions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.globalactions.xml \
     vendor/lineage/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.profiles.xml \

Bianca-Project/vendor_lineage

config/lineage_sdk_common.mk
@@ -1,6 +1,7 @@
 # Permissions for lineage sdk services
 PRODUCT_COPY_FILES += \
     vendor/bianca/config/permissions/org.lineageos.audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.audio.xml \
+    vendor/bianca/config/permissions/org.lineageos.globalactions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.globalactions.xml \
     vendor/bianca/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.hardware.xml \
     vendor/bianca/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/bianca/config/permissions/org.lineageos.performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.performance.xml \