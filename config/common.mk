2fe8231c9bb720fe2e598068f73e3f90f93b1b0f
cjybyjk <cjybyjk@zjnu.edu.cn>
2021-08-25 12:25:44 +0000

    Add GamingMode support [2/3]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I78351e1e8e49a4293fa0337fe49ed7786f98edf3
    Signed-off-by: alanndz <alanndz@qmail.id>

--- config/common.mk
+++ config/common.mk
@@ -123,6 +123,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Terminal
 
+# GamingMode
+PRODUCT_PACKAGES += \
+    GamingMode
+
 # Lineage packages
 PRODUCT_PACKAGES += \
     LineageParts \
