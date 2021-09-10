b1ac4cc43f9fbcf120ce86e29f29102048495dec
alanndz <alanndz@qmail.id>
2021-09-10 01:07:46 +0000

    vendor: Update to September 2021 fingerprint
    Signed-off-by: alanndz <alanndz@qmail.id>

--- config/common.mk
+++ config/common.mk
@@ -182,7 +182,7 @@ PRODUCT_PACKAGES += \
     mount.ntfs
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 # Openssh
 PRODUCT_PACKAGES += \
