0583a18cdf1d7a3a8ed6454748df6fbf0a84cb7f
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-24 23:28:33 +0530

    Add dudu target
    * Reference:
      https://github.com/AOSiP/platform_vendor_aosip

---
+++ build/tasks/bianca.mk
@@ -0,0 +1,7 @@
+CUSTOM_ROM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(BIANCA_VERSION).zip
+
+.PHONY: dudu
+dudu: otapackage
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_ROM_TARGET_PACKAGE)
+	@echo -e "zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
+	@echo -e "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
