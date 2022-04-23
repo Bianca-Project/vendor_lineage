7ae967f5d6fec63fd20fb15f6b85ff0b1b776cad
alanndz <alanndz7@gmail.com>
2022-04-23 23:28:29 +0000

    vendor: Update bootanimation for bianca
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2020 The Proton AOSP Project
+# Copyright (C) 2021 Bianca Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,5 +14,7 @@
 # limitations under the License.
 
 # Boot animation
+TARGET_SCREEN_WIDTH ?= 1080
+TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     bootanimation.zip
