8ed5d2e9c53d24fbbc7387bd178e5fe5fb950927
alanndz <alanndz7@gmail.com>
2022-04-24 17:41:46 +0000

    tasks: Show md5sum after finished build
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- build/tasks/bianca.mk
+++ build/tasks/bianca.mk
@@ -5,3 +5,4 @@ dudu: otapackage
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_ROM_TARGET_PACKAGE)
 	@echo -e "zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
 	@echo -e "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
+	@echo -e "md5sum: `md5sum $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 1`"
