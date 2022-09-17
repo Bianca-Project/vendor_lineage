924eb230ee8763e81f9d26abadc62e5ecd673c1d
alanndz <alanndz7@gmail.com>
2022-09-17 12:17:40 +0000

    vendor: soong: Conditionally detection using prebuilt or inline kernel
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- config/BoardConfigBianca.mk
+++ config/BoardConfigBianca.mk
@@ -1,5 +1,10 @@
+SOONG_CONFIG_NAMESPACES += kernel_type
+SOONG_CONFIG_kernel_type += name
+SOONG_CONFIG_kernel_type_name ?= kernel_prebuilt
+
 ifeq ($(TARGET_KERNEL_INLINE),true)
 include vendor/bianca/config/BoardConfigKernel.mk
+SOONG_CONFIG_kernel_type_name := kernel_inline
 endif
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
