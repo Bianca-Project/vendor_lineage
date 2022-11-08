2b2c6b6288464d3c2bdcf807eaf7014994bf8c4d
alanndz <alanndz7@gmail.com>
2022-11-08 18:24:59 +0000

    vendor: Update fp redfin to November patch

--- build/main.mk
+++ build/main.mk
@@ -15,7 +15,7 @@
 # Signing
 -include vendor/bianca/signing/keys.mk
 
-PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:13/TP1A.221005.002/9012097:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:13/TP1A.221105.002/9080065:user/release-keys
 
 # Override fingerprint for Google Play Services and SafetyNet
 ifneq ($(PRODUCT_OVERRIDE_FINGERPRINT),)
