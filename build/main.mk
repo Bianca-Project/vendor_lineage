a100bc0e4c054e1f12845b8a3b5d69115cbd89b2
Mocarafee <harif.rp05@gmail.com>
2023-10-20 14:20:50 +0700

    vendor: Update redfin fingerprint to September 2023 sec patch

--- build/main.mk
+++ build/main.mk
@@ -15,7 +15,7 @@
 # Signing
 -include vendor/bianca/signing/keys.mk
 
-PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:13/TQ2A.230505.002/9891397:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:13/TQ3A.230901.001/10750268:user/release-keys
 
 # Override fingerprint for Google Play Services and SafetyNet
 ifneq ($(PRODUCT_OVERRIDE_FINGERPRINT),)
